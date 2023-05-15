7590222984e382c0657e269b081c794e38fabfa7
unknown <marlondaza@unicauca.edu.co>
2023-05-15 17:37:19 -0500

    terminado hasta el modulo 06

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -113,6 +113,11 @@ function verificarPassword(objetoUsuario, password) {
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
 
+   if(objetoUsuario.password===password) {
+      return true;
+   }
+   else return false;
+
 
 }
 
@@ -142,7 +147,13 @@ function pasarUsuarioAPremium(objetoMuchosUsuarios) {
    // Define esta propiedad de todos los usuarios como true.
    // Retornar el arreglo.
    // Tu código:
-   
+
+   for (let i= 0; i<objetoMuchosUsuarios.length; i++) {
+      objetoMuchosUsuarios[i].esPremium = true;
+   }
+
+   return objetoMuchosUsuarios;
+
 }
 
 function sumarLikesDeUsuario(objetoUsuario) {
@@ -151,6 +162,15 @@ function sumarLikesDeUsuario(objetoUsuario) {
    // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
    // Debes sumar los likes de todos los post y retornar el resultado.
    // Tu código:
+   var sumaLikes = 0;
+   arrayObjetos = objetoUsuario.posts;
+
+   for (let i=0; i<objetoUsuario.posts.length; i++) {
+      sumaLikes = sumaLikes + arrayObjetos[i].likes;
+
+   }
+   return sumaLikes;
+
 }
 
 function agregarMetodoCalculoDescuento(objetoProducto) {
@@ -164,6 +184,9 @@ function agregarMetodoCalculoDescuento(objetoProducto) {
    // PorcentajeDeDescuento ---> 0.2
    // Precio final ---> 8
    // Tu código:
+
+   objetoProducto.calcularPrecioDescuento = function () { return this.precio-(this.precio * this.porcentajeDeDescuento)};
+   return objetoProducto;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
